af768a503f794dc9400191650bc301d3ebe804e1
Li Ilmira <ilmirali@protonmail.com>
2022-02-11 18:54:53 +0600

    little more than 'Hello, world'

---
+++ 0_hello_world_like.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+"fmt"
+"math")
+// int | float 32 | string | bool
+
+var i, a, b, c = 112 , 0.1, "Text", false
+
+//b string = 't'
+//c bool = false 
+
+func main(){
+// комментарий
+
+j := 7
+x,y := 10,20
+
+fmt.Println("Кириллица!")
+fmt.Println(math.Max(9,0))	
+fmt.Println(i)
+
+i = -1
+fmt.Println(i)
+
+i = 3
+fmt.Println(i,j,x,y)
+fmt.Println(a,b,c)
+}
+
+/* 
+comments
+ */
