f59e2d8d253de8084a83ba836e9842f29e168de5
Li Ilmira <ilmirali@protonmail.com>
2022-02-14 16:46:35 +0600

    Basic concepts

--- 0_1.go
+++ 0_1.go
@@ -6,14 +6,25 @@ import (
 // int | float 32 | string | bool
 
 var i, a, b, c = 112 , 0.1, "Text", false
+var a1 float32
+var a2 float64
+var b1 int
+var c1 bool
+var d1 string
+const r42 = 42
+
+
 
 //b string = 't'
 //c bool = false 
 
 func main(){
 // комментарий
+var j2 = 23
 
-j := 7
+fmt.Println(r42)
+
+j := j2 + 7
 x,y := 10,20
 
 fmt.Println("Кириллица!")
@@ -26,6 +37,14 @@ fmt.Println(i)
 i = 3
 fmt.Println(i,j,x,y)
 fmt.Println(a,b,c)
+
+fmt.Println("---")
+
+fmt.Println("a1", a1)
+fmt.Println("a2", a2)
+fmt.Println("b1", b1)
+fmt.Println("c1", c1)
+fmt.Println("d1", d1)
 }
 
 /* 
