b82391114b01bcb1703681dda1dfc61e4feb4118
Li Ilmira <ilmirali@protonmail.com>
2022-02-16 16:08:18 +0600

    Simple switch about

---
+++ 0_10.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x,y,z int
+    
+    fmt.Scan(&x)
+    fmt.Scan(&y)
+    fmt.Scan(&z)
+
+    switch{
+	case x==0:     fmt.Println("Ноль")
+        case x==1:     fmt.Println("Один")
+        case x==2:     fmt.Println("Два")
+        case x==3:     fmt.Println("Три")
+        case x==4:     fmt.Println("Чеьыре")
+        case x==5:     fmt.Println("Пять")
+        case x==6:     fmt.Println("Шесть")
+        case x==7:     fmt.Println("Семь")
+        case x==8:     fmt.Println("Восемь")
+        case x==9:     fmt.Println("Девять")
+        case x==10:    fmt.Println("Десять")
+    }
+
+    switch{
+	case y==0:     fmt.Println("Ноль")
+        case y==1:     fmt.Println("Один")
+        case y==2:     fmt.Println("Два")
+        case y==3:     fmt.Println("Три")
+        case y==4:     fmt.Println("Чеьыре")
+        case y==5:     fmt.Println("Пять")
+        case y==6:     fmt.Println("Шесть")
+        case y==7:     fmt.Println("Семь")
+        case y==8:     fmt.Println("Восемь")
+        case y==9:     fmt.Println("Девять")
+        case y==10:    fmt.Println("Десять")
+    }
+
+    switch{
+	case z==0:     fmt.Println("Ноль")
+        case z==1:     fmt.Println("Один")
+        case z==2:     fmt.Println("Два")
+        case z==3:     fmt.Println("Три")
+        case z==4:     fmt.Println("Чеьыре")
+        case z==5:     fmt.Println("Пять")
+        case z==6:     fmt.Println("Шесть")
+        case z==7:     fmt.Println("Семь")
+        case z==8:     fmt.Println("Восемь")
+        case z==9:     fmt.Println("Девять")
+        case z==10:    fmt.Println("Десять")
+    }
+
+
+}
