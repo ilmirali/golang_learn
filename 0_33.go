7fec50d6f80a16e6732999a30f59781acdfed3df
Li Ilmira <ilmirali@protonmail.com>
2022-02-25 16:01:23 +0600

    Variable arguments functions

---
+++ 0_33.go
@@ -0,0 +1,18 @@
+//Variable arguments functions
+package main
+
+import "fmt"
+
+func sum(nums ...int) {
+	total := 0
+	for _, v := range nums{
+		total += v
+	}
+	fmt.Println(total)
+}
+
+func main() {
+sum(2,4,6,8)
+sum(42,8)
+sum(1,2,3,4,5,6)
+}
