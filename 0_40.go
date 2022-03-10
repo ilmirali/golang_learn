456675a095726c87463d5cecd2ec1fa2cb79cadd
Li Ilmira <ilmirali@protonmail.com>
2022-03-10 14:39:54 +0600

    Channels testing

---
+++ 0_40.go
@@ -0,0 +1,20 @@
+// Channels testing
+
+package main
+
+import "fmt"
+
+func f(ch chan int) {
+	ch <- 8
+}
+
+func main() {
+	ch := make(chan int)
+	go f(ch)
+	select {
+		case <- ch:
+			fmt.Println("a")
+		default:
+			fmt.Println("b")
+	}
+}
